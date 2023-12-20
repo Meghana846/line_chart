1030f4cb0b712b1a880805a4a11b1f1d359761fc
M. Meghana <56499931+Meghana846@users.noreply.github.com>
2023-12-20 10:40:03 +0530

    Add files via upload
    working csv example file

--- main.dart
+++ main.dart
@@ -1,8 +1,7 @@
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:csv/csv.dart';
-import 'dart:io';
-import 'my_button.dart';
+import 'dart:typed_data' show Uint8List;
+import 'package:flutter/services.dart' show ByteData, rootBundle;
 
 void main() {
   runApp(const MainApp());
@@ -18,117 +17,64 @@ class MainApp extends StatelessWidget {
       appBar: AppBar(
         title: Align(
           alignment: Alignment.center,
-          child: const Text('Flutter Line Chart'),
+          child: const Text('Flutter CSV Reader'),
         ),
       ),
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            HeartRate(), // Use the first widget
-            SizedBox(height: 30), // Add some spacing
             FutureBuilder(
               // Use FutureBuilder to read data from CSV file asynchronously
-              future: _readCSVData('path/to/your/csv/file.csv'),
+              future: _readCSVData(),
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.done) {
                   if (snapshot.hasError) {
                     // Display an error message if an error occurred
                     return Text('Error loading data: ${snapshot.error}');
                   }
-                  // If data is loaded, create LineChart
-                  List<List<double>>? data =
-                  snapshot.data as List<List<double>>?;
-                  return data != null
-                      ? NewWidget(data: data)
-                      : Text('Data is null');
+                  // If data is loaded, display the values of the first column
+                  List<double> data = snapshot.data as List<double>;
+                  return Column(
+                    children: data
+                        .map((value) => Text(value.toString()))
+                        .toList(),
+                  );
                 } else {
                   // Display a loading indicator while data is being loaded
                   return CircularProgressIndicator();
                 }
               },
-            ), // Use the second widget
+            ),
           ],
         ),
       ),
     ),
   );
 
-  static Future<List<List<double>>> _readCSVData(String filePath) async {
+  static Future<List<double>> _readCSVData() async {
+    // Read the CSV file from the assets
+    final ByteData data = await rootBundle.load('assets/output1.csv');
+    final List<List<dynamic>> rows = const CsvToListConverter().convert(
+      String.fromCharCodes(Uint8List.view(data.buffer)),
+      shouldParseNumbers: true,
+    );
 
-    final File file = File('assests/output.csv');
-    List<List<double>> data = [];
-
-    // Read the CSV file
-    String contents = await file.readAsString();
-    List<List<dynamic>> rowsAsListOfValues = const CsvToListConverter()
-        .convert(contents, shouldParseNumbers: true);
-
-    // Use a counter for incrementing x-values
-    int counter = 1;
-
-    for (var row in rowsAsListOfValues.take(100)) {
-      double yValue = double.parse(row[0].toString()); // y-values are in the first column
-      data.add([counter.toDouble(), yValue]);
-      counter++;
+    // Extract values from the first column
+    List<double> columnValues = [];
+    for (var row in rows) {
+      if (row.isNotEmpty) {
+        try {
+          double value = double.parse(row[0].toString());
+          columnValues.add(value);
+        } catch (e) {
+          print('Error parsing value: ${row[0]}');
+        }
+      }
     }
 
     // Print the extracted data
-    print('Extracted Data: $data');
-    return data;
-  }
-}
-
-class NewWidget extends StatelessWidget {
-  final List<List<double>> data;
-  const NewWidget({required this.data});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 300,
-      width: 450,
-      child: LineChart(
-        LineChartData(
-          minX: 0,
-          maxX: 300,
-          minY: 0,
-          maxY: 300,
-          gridData: FlGridData(
-            show: true,
-            getDrawingHorizontalLine: (value) {
-              return FlLine(
-                color: const Color(0xff37434d),
-              );
-            },
-            getDrawingVerticalLine: (value) {
-              return FlLine(
-                  color: const Color(0xff37434d), strokeWidth: 0.8);
-            },
-          ),
-          borderData: FlBorderData(
-            show: true,
-            border: Border.all(color: const Color(0xff37434d), width: 1),
-          ),
-          titlesData: FlTitlesData(
-            topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-            rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-          ),
-          lineBarsData: [
-            LineChartBarData(
-                spots: data
-                    .map((point) => FlSpot(point[0], point[1]))
-                    .toList(),
-                //isCurved: true,
-                color: const Color(0xff02d39a),
-                barWidth: 3,
-                //dotData: FlDotData(show: false),
-                belowBarData: BarAreaData(
-                  show: true,
-                )),
-          ],
-        ),
-      ),
-    );
+    print('Extracted Data: $columnValues');
+    return columnValues;
   }
 }
